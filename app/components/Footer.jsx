0fed4e212eca8318b9e676a61c0ba40925a0d982
riteshk-007 <codeshorts007@gmail.com>
2024-03-10 13:29:35 +0530

    Refactor Footer component and remove unused imports

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,66 +1,159 @@
+"use client";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaWhatsapp,
+} from "react-icons/fa6";
+import { useForm, ValidationError } from "@formspree/react";
+import { useState, useEffect } from "react";
+import Button from "./Button";
 
-const Footer = () => {
+export default function Footer() {
+  const [state, handleSubmit] = useForm("mayrjgkz");
+  const [showSuccess, setShowSuccess] = useState(false);
+  useEffect(() => {
+    if (state.succeeded) {
+      setShowSuccess(true);
+      setTimeout(() => {
+        setShowSuccess(false);
+      }, 5000);
+    }
+  }, [state.succeeded]);
+
   return (
-    <div className="w-full bg-black mx-auto flex items-center justify-center md:p-5">
-      <div className="bg-black md:w-3/4 text-white p-6 md:flex md:justify-between md:items-center px-10 flex-wrap">
-        <Link
-          href={"/"}
-          className="mb-4 md:mb-0 mx-auto md:mx-0  flex items-center justify-center"
-        >
-          <Image width={80} height={80} src="/logo.png" alt="Company Logo" />
-        </Link>
-        <div className="mb-10 mx-auto md:mx-0 md:w-auto w-full text-center md:text-start">
-          <p>Email: info@conture.com</p>
+    <footer className="bg-gray-900 text-white py-8">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex flex-wrap justify-between items-center">
+        {/* Left section */}
+        <div className="flex items-center w-full sm:w-auto mb-8 sm:mb-0">
+          <Image
+            width={90}
+            height={90}
+            src="/logo.png"
+            alt="Company Logo"
+            className=""
+          />
+          <div className="ml-4">
+            <p className="font-semibold text-sm text-gray-400">
+              rajapuri, uttam nagar, New delhi - 110059,
+            </p>
+            <p className="font-semibold text-sm text-gray-400"> Delhi India</p>
+            <p className="font-semibold text-sm text-gray-400">
+              +91 74250 81690
+            </p>
+          </div>
         </div>
-        <div className="mb-4 md:mb-0 mx-auto md:mx-0 md:w-auto w-full text-center md:text-start">
-          <ul className="space-y-2">
-            <li>
-              <Link
-                href="/price"
-                className="text-[#FF1493] hover:underline transition duration-200"
-              >
+
+        {/* Middle section */}
+        <div className="text-start w-full sm:w-auto mb-8 sm:mb-0">
+          <p className="font-semibold text-sm text-gray-400">NAVIGATION</p>
+          <ul className="mt-2">
+            <li className="mb-2">
+              <Link href="/" className="text-white hover:underline">
+                Home
+              </Link>
+            </li>
+            <li className="mb-2">
+              <Link href="/price" className="text-white hover:underline">
                 Price
               </Link>
             </li>
-            <li>
-              <Link
-                href="/contact"
-                className="text-[#FF1493] hover:underline transition duration-200"
-              >
-                Contact Us
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/blogs"
-                className="text-[#FF1493] hover:underline transition duration-200"
-              >
-                Blogs
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/faq"
-                className="text-[#FF1493] hover:underline transition duration-200"
-              >
+            <li className="mb-2">
+              <Link href="/faq" className="text-white hover:underline">
                 FAQ
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" className="text-white hover:underline">
+                Contact Us
               </Link>
             </li>
           </ul>
         </div>
-        <hr className="my-4  w-full " />
-        <div className="mx-auto md:mx-0 w-full text-center md:flex space-y-2 md:space-y-0 md:space-x-3">
-          <p className="text-gray-400 text-sm cursor-pointer">Privacy Policy</p>
-          <p className="text-gray-400 text-sm cursor-pointer">
-            Terms of Service
-          </p>
-          <p>Copyrights reserved by Conture | Conture AI</p>
+
+        {/* Right section */}
+        <div className="text-center w-full sm:w-auto">
+          <p className="font-semibold text-sm text-gray-400">NEWSLETTER</p>
+          {showSuccess ? (
+            <div className="w-full mx-auto flex items-center justify-center p-2">
+              <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative">
+                <strong className="font-bold">Success!</strong>
+                <span className="block sm:inline">
+                  Your message has been sent successfully.
+                </span>
+              </div>
+            </div>
+          ) : (
+            <form
+              onSubmit={handleSubmit}
+              className="flex items-center justify-center w-full flex-col space-y-2"
+            >
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="p-2 border border-gray-300 w-full  rounded text-black bg-gray-200"
+                required
+                name="email"
+              />
+              <ValidationError
+                prefix="email"
+                field="email"
+                errors={state.errors}
+              />
+              <Button
+                className="border border-gray-700 w-full md:w-1/2"
+                variant="default"
+                type="submit"
+                text={state.submitting ? "SUBSCRIBING..." : "SUBSCRIBE"}
+              />
+            </form>
+          )}
         </div>
       </div>
-    </div>
-  );
-};
 
-export default Footer;
+      {/* Copyright */}
+      <div className="text-center flex flex-col sm:flex-row items-center justify-evenly mt-8 border-t border-gray-500 p-5">
+        <p variant="caption" className="text-gray-500 mb-4 sm:mb-0">
+          © 2024 Feel Right Inc. All Rights Reserved.
+        </p>
+        <div className="flex justify-center space-x-4">
+          <a
+            href="https://www.facebook.com/profile.php?id=61556161540988"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block  text-white p-1 transition-colors duration-300 ease-in-out"
+          >
+            <FaFacebook className="w-6 h-6" />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/conture-media/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block  text-white p-1 transition-colors duration-300 ease-in-out"
+          >
+            <FaLinkedin className="w-6 h-6" />
+          </a>
+          <a
+            href="https://wa.me/+917425081690"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block  text-white p-1 transition-colors duration-300 ease-in-out"
+          >
+            <FaWhatsapp className="w-6 h-6" />
+          </a>
+          <a
+            href="https://www.instagram.com/conture.ai"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block  text-white p-1 transition-colors duration-300 ease-in-out"
+          >
+            <FaInstagram className="w-6 h-6" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- app/components/Mail.jsx
+++ app/components/Mail.jsx
@@ -1,6 +1,5 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { useForm, ValidationError } from "@formspree/react";
 import { useState, useEffect } from "react";
 
