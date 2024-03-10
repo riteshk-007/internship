0789d62c15f2fd027705e78e61a88fb6e8f60f0d
riteshk-007 <codeshorts007@gmail.com>
2024-03-10 14:33:13 +0530

    Remove MapComponent from Contact page

--- app/contact/Contact.jsx
+++ app/contact/Contact.jsx
@@ -1,10 +1,8 @@
 "use client";
-
+import React, { useState, useEffect } from "react";
 import { useForm, ValidationError } from "@formspree/react";
-import { useState, useEffect } from "react";
-import SecondButton from "../components/secondButton";
 
-const ContactForm = () => {
+const Contactform = () => {
   const [state, handleSubmit] = useForm("mayrjgkz");
   const [showSuccess, setShowSuccess] = useState(false);
 
@@ -20,7 +18,7 @@ const ContactForm = () => {
   if (showSuccess) {
     return (
       <div className="w-full mx-auto flex items-center justify-center p-2">
-        <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative">
+        <div className="bg-pink-100 border border-pink-400 text-pink-700 px-4 py-3 rounded relative">
           <strong className="font-bold">Success!</strong>
           <span className="block sm:inline">
             Your message has been sent successfully.
@@ -30,71 +28,187 @@ const ContactForm = () => {
     );
   }
   return (
-    <div className="w-full p-5  flex flex-col items-center justify-center ">
-      <form
-        onSubmit={handleSubmit}
-        className="w-full max-w-md mx-auto bg-black/50 rounded-md   p-5 md:p-10"
+    <section>
+      <div
+        id="map"
+        className="relative h-[350px] overflow-hidden bg-cover bg-[50%] bg-no-repeat"
       >
-        <div className="mb-4">
-          <label
-            className="block text-gray-200 text-sm font-bold mb-2"
-            htmlFor="name"
-          >
-            Name
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:ring-2 focus:ring-purple-500"
-            id="name"
-            type="text"
-            placeholder="Your name"
-            name="name"
-            required
-          />
-          <ValidationError prefix="name" field="name" errors={state.errors} />
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d925134.3210715873!2d54.568041327437584!3d25.0745656650172!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f43496ad9c645%3A0xbde66e5084295162!2sDubai%20-%20United%20Arab%20Emirates!5e0!3m2!1sen!2sin!4v1710061283002!5m2!1sen!2sin"
+          width="100%"
+          height={480}
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+        />
+      </div>
+      <div className="container px-6 md:px-10">
+        <div className="block rounded-lg bg-gray-950  px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]  md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px]  border-gray-300 dark:border-gray-800 border">
+          <div className="flex flex-wrap">
+            <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:px-3 lg:mb-0 lg:w-5/12 lg:px-6">
+              <form onSubmit={handleSubmit}>
+                <div className="relative mb-6" data-te-input-wrapper-init="">
+                  <input
+                    className="peer block min-h-[auto] w-full rounded border bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none "
+                    id="exampleInput90"
+                    type="text"
+                    name="name"
+                    required
+                  />
+                  <label
+                    className="pointer-events-none  absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-300 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
+                    htmlFor="exampleInput90"
+                  >
+                    Name
+                  </label>
+                  <ValidationError
+                    prefix="name"
+                    field="name"
+                    errors={state.errors}
+                  />
+                </div>
+                <div className="relative mb-6" data-te-input-wrapper-init="">
+                  <input
+                    className="peer block min-h-[auto] w-full rounded border bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none "
+                    id="exampleInput91"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                  <label
+                    className="pointer-events-none  absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-300 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
+                    htmlFor="exampleInput91"
+                  >
+                    Email address
+                  </label>
+                  <ValidationError
+                    prefix="email"
+                    field="email"
+                    errors={state.errors}
+                  />
+                </div>
+                <div className="relative mb-6" data-te-input-wrapper-init="">
+                  <textarea
+                    className="peer block min-h-[auto] w-full rounded border bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none "
+                    id="exampleFormControlTextarea1"
+                    rows={3}
+                    name="message"
+                    required
+                  />
+                  <label
+                    htmlFor="exampleFormControlTextarea1"
+                    className="pointer-events-none  absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-300 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
+                  >
+                    Message
+                  </label>
+                  <ValidationError
+                    prefix="message"
+                    field="message"
+                    errors={state.errors}
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-full bg-pink-500 dark:bg-pink-600 text-white  py-3 rounded-md hover:bg-pink-500 transition-all duration-200 ease-linear focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:ring-offset-neutral-100 focus:ring-opacity-50"
+                >
+                  send message
+                </button>
+              </form>
+            </div>
+            <div className="w-full shrink-0 grow-0 basis-auto lg:w-7/12">
+              <div className="flex flex-wrap">
+                <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:w-full lg:px-6 xl:w-6/12">
+                  <div className="flex items-start">
+                    <div className="shrink-0">
+                      <div className="inline-block rounded-md bg-pink-500 p-4 text-primary">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={2}
+                          stroke="currentColor"
+                          className="h-6 w-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0l6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 014.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 00-.38 1.21 12.035 12.035 0 007.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 011.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 01-2.25 2.25h-2.25z"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="ml-6 grow text-gray-300">
+                      <p className="mb-2 font-bold text-sm ">
+                        Technical support
+                      </p>
+                      <p className="text-sm ">conture@gmail.com</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:w-full lg:px-6 xl:w-6/12">
+                  <div className="flex items-start">
+                    <div className="srink-0">
+                      <div className="inline-block rounded-md bg-pink-500 p-4 text-primary">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={2}
+                          stroke="currentColor"
+                          className="w-7 h-7"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="ml-6 grow text-gray-300">
+                      <p className="mb-2 font-bold text-sm ">Address</p>
+                      <p className="text-xs text-gray-300">
+                        New York City, 1234 - 5678 - 90
+                        <br />
+                        United States
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:w-full lg:px-6 xl:mb-12 xl:w-6/12">
+                  <div className="align-start flex">
+                    <div className="shrink-0">
+                      <div className="inline-block rounded-md bg-pink-500 p-4 text-primary">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="ml-6 grow text-gray-300">
+                      <p className="mb-2 font-bold text-sm ">Mobile</p>
+                      <p className="text-gray-300 text-xs"> +91 74250 81690</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-200 text-sm font-bold mb-2"
-            htmlFor="email"
-          >
-            Email
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:ring-2 focus:ring-purple-500"
-            id="email"
-            type="email"
-            placeholder="Your email"
-            name="email"
-            required
-          />
-          <ValidationError prefix="email" field="email" errors={state.errors} />
-        </div>
-        <div className="mb-6">
-          <label
-            className="block text-gray-200 text-sm font-bold mb-2"
-            htmlFor="message"
-          >
-            Message
-          </label>
-          <textarea
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:ring-2 focus:ring-purple-500"
-            id="message"
-            placeholder="Your message"
-            name="message"
-            required
-          />
-          <ValidationError
-            prefix="message"
-            field="message"
-            errors={state.errors}
-          />
-        </div>
-        <div className="flex items-center justify-center">
-          <SecondButton text="Send Message" />
-        </div>
-      </form>
-    </div>
+      </div>
+    </section>
   );
 };
-
-export default ContactForm;
+export default Contactform;
