f83f8908e213db43024a51eed6ea295819941bc2
riteshk-007 <codeshorts007@gmail.com>
2024-03-11 11:35:32 +0530

    Update form labels and placeholders

--- app/contact/Contact.jsx
+++ app/contact/Contact.jsx
@@ -48,19 +48,21 @@ const Contactform = () => {
             <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:px-3 lg:mb-0 lg:w-5/12 lg:px-6">
               <form onSubmit={handleSubmit}>
                 <div className="relative mb-6" data-te-input-wrapper-init="">
+                  <label
+                    className="pointer-events-none   mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-300 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
+                    htmlFor="exampleInput90"
+                  >
+                    Name
+                  </label>
                   <input
                     className="peer block min-h-[auto] w-full rounded border bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none "
                     id="exampleInput90"
                     type="text"
                     name="name"
+                    placeholder=""
                     required
                   />
-                  <label
-                    className="pointer-events-none  absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-300 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
-                    htmlFor="exampleInput90"
-                  >
-                    Name
-                  </label>
+
                   <ValidationError
                     prefix="name"
                     field="name"
@@ -68,19 +70,21 @@ const Contactform = () => {
                   />
                 </div>
                 <div className="relative mb-6" data-te-input-wrapper-init="">
+                  <label
+                    className="pointer-events-none   mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-300 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
+                    htmlFor="exampleInput91"
+                  >
+                    Email address
+                  </label>
                   <input
                     className="peer block min-h-[auto] w-full rounded border bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none "
                     id="exampleInput91"
                     type="email"
                     name="email"
+                    placeholder="Email"
                     required
                   />
-                  <label
-                    className="pointer-events-none  absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-300 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
-                    htmlFor="exampleInput91"
-                  >
-                    Email address
-                  </label>
+
                   <ValidationError
                     prefix="email"
                     field="email"
@@ -88,19 +92,21 @@ const Contactform = () => {
                   />
                 </div>
                 <div className="relative mb-6" data-te-input-wrapper-init="">
+                  <label
+                    htmlFor="exampleFormControlTextarea1"
+                    className="pointer-events-none   mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-300 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
+                  >
+                    Message
+                  </label>
                   <textarea
                     className="peer block min-h-[auto] w-full rounded border bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none "
                     id="exampleFormControlTextarea1"
                     rows={3}
                     name="message"
+                    placeholder="Message"
                     required
                   />
-                  <label
-                    htmlFor="exampleFormControlTextarea1"
-                    className="pointer-events-none  absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-300 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
-                  >
-                    Message
-                  </label>
+
                   <ValidationError
                     prefix="message"
                     field="message"
